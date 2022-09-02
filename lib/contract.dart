f3cd9931fdd6c68e58a9364643edaedeef7af713
Iulian Blajut <iulian.blajut@gmail.com>
2022-09-03 00:12:36 +0300

    Refactor builtin contracts

--- lib/contract.dart
+++ lib/contract.dart
@@ -3,5 +3,6 @@ library contract;
 export 'src/contract/abi_entries.dart';
 export 'src/contract/contract.dart';
 export 'src/contract/contract_abi.dart';
+export 'src/contract/contracts.dart';
 export 'src/contract/solidity_types.dart';
 export 'src/contract/types.dart';
